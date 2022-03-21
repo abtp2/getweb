e1eb150ec096f844a8afc9862617d89068d6a0da
Ashutosh Pandey <94055977+abtp2@users.noreply.github.com>
2022-03-21 23:10:45 +0530

    Add files via upload

---
+++ htmlquizquestion.js
@@ -0,0 +1,123 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "A webpage displays a picture. What tag was used to display that picture?",
+    answer: "img",
+    options: [
+      "picture",
+      "image",
+      "img",
+      "src"
+    ]
+  },
+    {
+    numb: 3,
+    question: "<b> tag makes the enclosed text bold. What is other tag to make text bold?",
+    answer: "<strong>",
+    options: [
+      "&ltstrong&gt",
+      "&ltdar&gt",
+      "&ltblack&gt",
+      "&ltemp&gt"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Tags and test that are not directly displayed on the page are written in _____ section.",
+    answer: "<head>",
+    options: [
+      "&lthtml&gt",
+      "&lthead&gt",
+      "&lttitle&gt",
+      "&ltbody&gt"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which tag inserts a line horizontally on your web page?",
+    answer: "<hr>",
+    options: [
+      "&lthr&gt",
+      "&ltline&gt",
+      "&ltline direction=”horizontal”&gt",
+      "&lttr&gt"
+    ]
+  },
+ // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+     {
+     numb: 6,
+   question: "What should be the first tag in any HTML document?",
+  answer: "<html>",
+   options: [
+   "&lthtml&gt",
+     "&lttitle&gt",
+      "&ltbody&gt",
+       "&lthead&gt"
+     ]
+   },
+   
+        {
+     numb: 7,
+   question: "Which tag allows you to add a row in a table?",
+  answer: "<tr>",
+   options: [
+   "&lttrow&gt",
+     "&ltth&gt",
+      "&lttr&gt",
+       "&lttd&gt"
+     ]
+   },
+   
+   
+        {
+     numb: 8,
+   question: "How can you make a bulleted list?",
+  answer: "<ul>",
+   options: [
+   "&ltlist&gt",
+     "&ltnl&gt",
+      "&ltul&gt",
+       "&ltol&gt"
+     ]
+   },
+   
+        {
+     numb: 9,
+   question: "How can you make a numbered list?",
+  answer: "<ol>",
+   options: [
+   "&ltdl&gt",
+     "&ltol&gt",
+      "&ltul&gt",
+       "&ltlist&gt"
+     ]
+   },
+   
+        {
+     numb: 10,
+   question: "How can you make an e-mail link?",
+  answer: "<a href=”mailto:xyx@yxy”>",
+   options: [
+   "&lta href=”xyx@yxy”&gt",
+     "&ltmail href=”xyx@yzy”&gt",
+      "&ltmail&gtxyx@yxy&lt/mail&gt",
+       "&lta href=”mailto:xyx@yxy”&gt"
+     ]
+   },
+   
+   
+];
