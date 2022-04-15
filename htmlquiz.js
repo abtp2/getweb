0c1082987b1a06e83ee538ce6715e33af76b64e1
Ashutosh Pandey <94055977+abtp2@users.noreply.github.com>
2022-04-15 23:54:45 +0530

    Add files via upload

--- htmlquiz.js
+++ htmlquiz.js
@@ -150,7 +150,7 @@ function showResult(){
     quiz_box.classList.remove("activeQuiz"); //hide quiz box
     result_box.classList.add("activeResult"); //show result box
     const scoreText = result_box.querySelector(".score_text");
-    if (userScore > 3){ // if user scored more than 3
+    if (userScore > 9){ // if user scored more than 3
         //creating a new span tag and passing the user score number and total question number
         let scoreTag = '<span>Your Certificate Passkey is gsoi2atp</span>';
         scoreText.innerHTML = scoreTag;  //adding new span tag inside score_Text
